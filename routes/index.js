ed74331e6ac1bcdb42b98d119393bd3993a52bd1
afd <afd@alup5.academiapostal.es.local>
2020-01-28 20:56:14 +0100

    Edición con Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const productos = [
+    {nombre: "Microondas", precio:45, existencias: 6},
+    {nombre: "Frigorífico", precio:200, existencias: 4},
+    {nombre: "TV", precio:90, existencias: 0},
+    {nombre: "Lámpara", precio:20, existencias: 14},
+    {nombre: "Lavadora", precio:290, existencias: 3},
+    {nombre: "Secadora", precio:180, existencias: 5},
+    ];
+  res.render('index', { title: 'Dood', productos });
 });
 
 module.exports = router;

bcad8e8a9a8c2c0c9649a4b1ea824cf6f56c045c
afd <afd@alup5.academiapostal.es.local>
2020-01-29 19:36:06 +0100

    Crear archivo para los productos

---
+++ models/products.js
@@ -0,0 +1,9 @@
+// module.exports hace que todo lo que contenga se pueda acceder desde cualquier otra parte del proyecto
+module.exports = [
+    {nombre: "Microondas", precio:45, existencias: 6, imagen: "microondas.jpg"},
+    {nombre: "Frigorífico", precio:200, existencias: 4, imagen: "frigorífico.jpg"},
+    {nombre: "TV", precio:90, existencias: 0, imagen: "tv.jpg"},
+    {nombre: "Lámpara", precio:20, existencias: 14, imagen: "lampara.jpg"},
+    {nombre: "Lavadora", precio:290, existencias: 3, imagen: "lavadora.jpg"},
+    {nombre: "Secadora", precio:180, existencias: 5, imagen: "secadora.jpg"},
+    ];
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+// require obtiene los elementos de la carpeta que se le indique ("AQUI"), siempre que tengan un module.exports
+// los mete dentro de la variable (productos) en este caso
+var productos = require("../models/products.js")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const productos = [
-    {nombre: "Microondas", precio:45, existencias: 6, imagen: "microondas.jpg"},
-    {nombre: "Frigorífico", precio:200, existencias: 4, imagen: "frigorífico.jpg"},
-    {nombre: "TV", precio:90, existencias: 0, imagen: "tv.jpg"},
-    {nombre: "Lámpara", precio:20, existencias: 14, imagen: "lampara.jpg"},
-    {nombre: "Lavadora", precio:290, existencias: 3},
-    {nombre: "Secadora", precio:180, existencias: 5},
-    ];
   res.render('index', { title: 'Dood', productos });
 });
 

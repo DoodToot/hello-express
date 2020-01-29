1efee3f42672da33ff874606704709f841df04aa
afd <afd@alup5.academiapostal.es.local>
2020-01-29 20:50:34 +0100

    Páginas de compra de cada producto usando el ref

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,17 @@ var productos = require("../models/products.js")
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dood', productos });
 });
-
+// creamos una nueva línea de (router) que direccione a /productos/:ref
+router.get('/productos/:ref', function (req, res, next) {
+  // indicamos que el ref de la dirección lo obtenemos de req (request)
+  var ref = req.params.ref;
+  console.log(ref);
+  // producto aisla la información del elemento que tenga el ref que buscamos
+  const producto = productos.find(function(p) {
+    return p.ref==ref;
+  });
+  console.log(producto);
+  // el render le indica que imprima el archivo product.ejs en esa dirección, y que utiliza la información recojida en (producto)
+  res.render('product', {producto});
+});
 module.exports = router;

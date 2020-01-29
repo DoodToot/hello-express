588f73e4eb014df6f0b1e70cbe1351e15768b683
afd <afd@alup5.academiapostal.es.local>
2020-01-29 18:21:17 +0100

    Añadir Imágenes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const productos = [
-    {nombre: "Microondas", precio:45, existencias: 6},
-    {nombre: "Frigorífico", precio:200, existencias: 4},
-    {nombre: "TV", precio:90, existencias: 0},
-    {nombre: "Lámpara", precio:20, existencias: 14},
+    {nombre: "Microondas", precio:45, existencias: 6, imagen: "microondas.jpg"},
+    {nombre: "Frigorífico", precio:200, existencias: 4, imagen: "frigorífico.jpg"},
+    {nombre: "TV", precio:90, existencias: 0, imagen: "tv.jpg"},
+    {nombre: "Lámpara", precio:20, existencias: 14, imagen: "lampara.jpg"},
     {nombre: "Lavadora", precio:290, existencias: 3},
     {nombre: "Secadora", precio:180, existencias: 5},
     ];

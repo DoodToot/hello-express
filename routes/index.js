053ccbb3b9dc68e7561e8621e3d58369cd591b6c
afd <afd@alup5.academiapostal.es.local>
2020-01-30 19:56:31 +0100

    Añadir una redirección y push al botón comprar

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,30 @@ router.get('/', function(req, res, next) {
 // creamos una nueva línea de (router) que direccione a /productos/:ref
 router.get('/productos/:ref', function (req, res, next) {
   // indicamos que el ref de la dirección lo obtenemos de req (request)
-  var ref = req.params.ref;
+  const ref = req.params.ref;
   console.log(ref);
   // producto aisla la información del elemento que tenga el ref que buscamos
   const producto = productos.find(function(p) {
     return p.ref==ref;
   });
   console.log(producto);
+  if (producto)
   // el render le indica que imprima el archivo product.ejs en esa dirección, y que utiliza la información recojida en (producto)
   res.render('product', {producto});
+  else res.redirect('/error');
 });
+
+const cesta = []; // provisional
+
+router.post('/comprar', function(req, res, next) {
+  const ref = req.params.ref;
+  const producto = productos.find(function(p) {
+    return p.ref==ref; 
+  });
+  // añadimos el producto a la cesta
+  cesta.push(producto);
+  // nos lleva a la página principal
+  res.redirect("/");
+});
+
 module.exports = router;
